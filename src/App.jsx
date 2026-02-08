9412048382f9c1120c05c53a6ebc0b73f033023b
Abdellah <abdellah.elkoulali@gmx.fr>
2026-02-08 21:06:04 +0100

    Move TrackingPgae into react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HomePage } from './assets/pages/HomePage';
 import { CheckoutPage } from './assets/pages/CheckoutPage';
 import { OrdersPage } from './assets/pages/OrdersPage';
+import { TrackingPage } from './assets/pages/TrackingPage';
 import { Routes, Route } from 'react-router';
 import './App.css'
 
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   )
 }
