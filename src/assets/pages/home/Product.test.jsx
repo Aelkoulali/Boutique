daadd43c2d2185ee42cd73fec685ad92b32386d4
Abdellah <abdellah.elkoulali@gmx.fr>
2026-02-14 22:01:08 +0100

    Use beforeEach to remove duplication

--- src/assets/pages/home/Product.test.jsx
+++ src/assets/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
@@ -7,18 +7,25 @@ import { Product } from "./Product";
 vi.mock("axios"); // Mock the axios module,
 
 describe('Product componenet', () => {
-    it('displays the product deatails correctly', () => {
-        const product = {
+    let  product ;
+    let loadCart; 
+    
+    beforeEach(() => {
+        product = {            
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
             rating: { stars: 4.5, count: 87 },   
             priceCents: 1090,
             keywords: ["socks", "sports", "apparel"]
-        };
+       };
 
-        const loadCart = vi.fn(); // Mock the loadCart function
+        loadCart = vi.fn(); // Mock the loadCart function
+         
+    });
 
+
+    it('displays the product deatails correctly', () => {   
         render(<Product product={product} loadCart={loadCart}/>);
         // Check if the product name is displayed
         expect(screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs")).toBeInTheDocument();
@@ -39,18 +46,6 @@ describe('Product componenet', () => {
 
        // Check id Adds a product to the cart when the "Add to Cart" button is clicked
        it('adds a product to the cart', async () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: { stars: 4.5, count: 87 },   
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
-        
-
-        const loadCart = vi.fn(); // Mock the loadCart function
-
         render(<Product product={product} loadCart={loadCart} />);
       
         const user = userEvent.setup();
